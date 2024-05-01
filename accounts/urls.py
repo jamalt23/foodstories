bf89660cf776d0d8dd4a129a3bd8d1fd273f27d3
jamalt23 <jamalt.live@gmail.com>
2024-05-01 17:38:39 +0400

    register verification

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('login/', login_auth, name="login"),
     path('user/<int:id>', profile, name="profile"),
     path('forgot-password/', ForgetPassword.as_view(), name="forgot-password"),
-    # path('success-password-request', success_password_request, name="success-password-request"),
     path('log-out', LogoutView.as_view(), name='log-out'),
     path('edit-profile/<int:pk>', EditProfile.as_view(), name="edit-profile"),
     path('password-reset-confirm/<str:uidb64>/<str:token>/', CustomPasswordResetConfirmView.as_view(), name = 'password-reset-confirm'),
     path('password-change/', CustomPasswordChangeView.as_view(), name = 'password-change'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', activate_account, name='activate'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,30 +6,59 @@ from django.core.mail import *
 from django.contrib.auth.views import *
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.http import HttpRequest
+from django.http import *
 from django.urls import reverse_lazy
 from django.core.exceptions import PermissionDenied
-
-# import random, string
-# def generate_random_token() -> str:
-#     return ''.join(random.sample(string.ascii_letters, 16))
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from accounts.tokens import account_activation_token
+from django.core.mail import EmailMessage
 
 def register(request: HttpRequest):
     categories = Category.objects.all()
     if request.method == 'POST':
         form = RegisterForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('accounts:login')
+            form.instance.is_active = False
+            user = form.save()
+            domain = get_current_site(request).domain
+            email_subject = "Activate your account"
+            message = render_to_string('activate-email.html', {
+                'user': user, 'domain': domain,
+                'uid': force_str(urlsafe_base64_encode(force_bytes(user.pk))),
+                'token': account_activation_token.make_token(user), 
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(email_subject, message, to=[to_email])
+            email.send()
+            context = {'categories': categories, 'sent_email' : True}
+            return render(request, 'login.html', context=context)
+
     form = RegisterForm()
     context = {
         'categories': categories, 'form': form
     }
     return render(request, 'register.html', context=context)
 
+def activate_account(request, uidb64, token):
+    try:
+        uid = force_bytes(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('core:home')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
 def login_auth(request: HttpRequest):
     categories = Category.objects.all()
-    context = {'categories': categories}
+    context = {'categories': categories, 'sent_email' : False}
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
